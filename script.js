051f137f526226d5d15b20473e29851a3facf480
NIRAJ <nirajpratimneil@gmail.com>
2026-07-24 00:25:03 +0530

    Initial Portfolio commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('a[href^="#"]').forEach(link =>{
+    link.addEventListener('click',function(e){e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+
+    });
+});
+
+
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = 'translateY(0)';
+
+        }
+    });
+},{threshold:0.5});
+
+sections.forEach(section =>{
+    section.style.opacity = 0;
+    section.style.transform = 'translateY(40px)';
+    section.style.transition = 'all 1.2s ease';
+    observer.observe(section);
+});
